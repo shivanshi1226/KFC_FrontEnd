126e2d83e5532ecfe26819b8dac39cfae49050b7
shivanshi1226 <shivanshishailja26@gmail.com>
2024-09-05 16:34:04 +0530

    changed css name

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import "./Checkout.css";
+import "./CheckOut.css";
 
 const Checkout = () => {
   const navigate = useNavigate();
